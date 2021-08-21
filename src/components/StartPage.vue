f4b0d42c14965ac512438420faeb00deab7f2337
Caleb Garratt <metal.carratt@gmail.com>
2021-08-21 14:10:37 +1200

    first commit

---
+++ src/components/StartPage.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="startPage">
+        <h1>Not Among Us</h1>
+        <form>
+            <fieldset>
+                <label>Join game:</label>
+                <input type="text" placeholder="Enter code" />
+                <button>Join Game</button>
+            </fieldset>
+            <fieldset>
+                <label>Or:</label>
+                <button>Host Game</button>
+            </fieldset>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    color: #f15555;
+    margin-top: 60px;
+    font-size: 60px;
+    text-shadow: 0px 0px 8px  #402121;
+    text-decoration: underline;
+}
+
+form {
+    width: 340px;
+    margin: 0 auto;
+    margin-top: 60px;
+}
+
+fieldset {
+    border: none;
+}
+
+label {
+    display: block;
+    font-size: 30px;
+    color: white;
+    margin-bottom: 15px;
+    margin-top: 15px;
+}
+
+input {
+    font-size: 20px;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #353569;
+    margin-bottom: 5px;
+    width: 100%;
+    box-sizing: border-box;
+}
+
+button {
+    width: 100%;
+    box-sizing: border-box;
+
+    font-size: 20px;
+    border-radius: 5px;
+    border: 1px solid #353569;
+    padding: 10px;
+    background-color: #fbcd07;
+}
+</style>
